0e8ad67ae6ad4e503321daf6d9fa6e463dfa0f24
tapu-gugu <415660840@qq.com>
2026-06-12 16:41:28 +0800

    Update rating_v2.ts

--- src/utils/rating_v2.ts
+++ src/utils/rating_v2.ts
@@ -221,7 +221,7 @@ export function calculateTaikoRating(
   const handspeed_rt = calcLnRating(rating, songData.handspeed, accuracy);
   */
   // const accuracy_rt = calcLnRating(rating, accuracy, accuracy);
-  const stamina_rt = calcSingleRating(accuracy, rating);
+  const accuracy_rt = calcSingleRating(accuracy, rating);
 
   const stamina_rt = calcSingleRating(songData.stamina, accuracy);
   const handspeed_rt = calcSingleRating(songData.handspeed, accuracy);
