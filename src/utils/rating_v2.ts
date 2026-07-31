ebb00238e3483b96e6d17800dc16b3fbfa792bbd
tapu-gugu <415660840@qq.com>
2026-07-31 16:50:58 +0800

    Update rating_v2.ts

--- src/utils/rating_v2.ts
+++ src/utils/rating_v2.ts
@@ -244,7 +244,11 @@ export function calculateTaikoRating(
   const burst_rt_base = calcSingleRating(songData.burst, accuracy);
   // const burst_hs_factor = songData.handspeed > 0 ? MIN(rating / songData.handspeed, 1) : 1;
   // const burst_hs_factor = songData.handspeed > 0 ? MIN(rating / songData.handspeed / MIN(rt_100 / songData.handspeed , 1) , 1) : 1;
+  */
+  
   const burst_hs_factor = songData.handspeed > 0 ? MIN(accuracy / songData.handspeed, 1) : 1;
+
+  /*
   // const burst_rt = burst_rt_base * burst_hs_factor;
   const burst_rt = burst_rt_base * burst_hs_factor > handspeed_rt ? handspeed_rt + MIN(MAX(accuracy - songData.handspeed, 0) / (songData.burst - songData.handspeed), 1) * (burst_rt_base * burst_hs_factor - handspeed_rt) : burst_rt_base * burst_hs_factor;
   */
